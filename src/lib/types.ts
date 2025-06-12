cd34df50bc90dafb59bc6e128da965a878d9acb6
Imon Chakraborty <chakraborty2005imon@gmail.com>
2025-06-12 22:07:34 +0530

    (ADD): Extend Movie and Series interfaces with additional properties for enhanced data representation

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -5,6 +5,14 @@ export interface Movie {
   poster_path: string | null;
   release_date: string;
   vote_average: number;
+  vote_count: number;
+  adult: boolean;
+  original_language: string;
+  original_title: string;
+  backdrop_path: string | null;
+  popularity: number;
+  video: boolean;
+  genre_ids: number[];
 }
 
 export interface Series {
@@ -14,6 +22,14 @@ export interface Series {
   poster_path: string | null;
   first_air_date: string;
   vote_average: number;
+  vote_count: number;
+  backdrop_path: string | null;
+  popularity: number;
+  original_name: string;
+  original_language: string;
+  origin_country: string[];
+  genre_ids: number[];
+  adult: boolean;
 }
 
 export interface TMDBResponse<T> {
@@ -21,4 +37,192 @@ export interface TMDBResponse<T> {
   results: T[];
   total_pages: number;
   total_results: number;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface ProductionCompany {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+export interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface SpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export interface CastMember {
+  id: number;
+  name: string;
+  character: string;
+  profile_path: string | null;
+  adult: boolean;
+  gender: number | null;
+  known_for_department: string;
+  original_name: string;
+  popularity: number;
+  cast_id?: number;
+  credit_id: string;
+  order: number;
+}
+
+export interface CrewMember {
+  id: number;
+  name: string;
+  job: string;
+  department: string;
+  profile_path: string | null;
+  adult: boolean;
+  gender: number | null;
+  known_for_department: string;
+  original_name: string;
+  popularity: number;
+  credit_id: string;
+}
+
+export interface Credits {
+  cast: CastMember[];
+  crew: CrewMember[];
+}
+
+export interface Video {
+  id: string;
+  iso_639_1: string;
+  iso_3166_1: string;
+  key: string;
+  name: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+}
+
+export interface Videos {
+  results: Video[];
+}
+
+export interface Collection {
+  id: number;
+  name: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+}
+
+export interface MovieDetails extends Omit<Movie, 'genre_ids'> {
+  belongs_to_collection: Collection | null;
+  budget: number;
+  genres: Genre[];
+  homepage: string | null;
+  imdb_id: string | null;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  revenue: number;
+  runtime: number | null;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string | null;
+  credits: Credits;
+  videos: Videos;
+}
+
+export interface CreatedBy {
+  id: number;
+  credit_id: string;
+  name: string;
+  original_name: string;
+  gender: number;
+  profile_path: string | null;
+}
+
+export interface Network {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+export interface Season {
+  air_date: string | null;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string | null;
+  season_number: number;
+  vote_average: number;
+}
+
+export interface LastEpisodeToAir {
+  id: number;
+  name: string;
+  overview: string;
+  vote_average: number;
+  vote_count: number;
+  air_date: string;
+  episode_number: number;
+  episode_type: string;
+  production_code: string;
+  runtime: number | null;
+  season_number: number;
+  show_id: number;
+  still_path: string | null;
+}
+
+export interface TVShowDetails extends Omit<Series, 'genre_ids'> {
+  created_by: CreatedBy[];
+  episode_run_time: number[];
+  genres: Genre[];
+  homepage: string | null;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string | null;
+  last_episode_to_air: LastEpisodeToAir | null;
+  next_episode_to_air: LastEpisodeToAir | null;
+  networks: Network[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  seasons: Season[];
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string | null;
+  type: string;
+  credits: Credits;
+  videos: Videos;
+}
+
+export interface Episode {
+  id: number;
+  name: string;
+  overview: string;
+  vote_average: number;
+  vote_count: number;
+  air_date: string | null;
+  episode_number: number;
+  episode_type: string;
+  production_code: string | null;
+  runtime: number | null;
+  season_number: number;
+  show_id: number;
+  still_path: string | null;
+  crew: CrewMember[];
+  guest_stars: CastMember[];
+}
+
+export interface SeasonDetails extends Season {
+  episodes: Episode[];
+  _id: string;
 }
