3cdcfc392af837cb3816ce78755442f9df61bd77
Imon Chakraborty <chakraborty2005imon@gmail.com>
2025-06-12 13:15:45 +0530

    (ADD): define Movie, Series, and TMDBResponse interfaces for type safety

---
+++ src/lib/types.ts
@@ -0,0 +1,24 @@
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string | null;
+  release_date: string;
+  vote_average: number;
+}
+
+export interface Series {
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string | null;
+  first_air_date: string;
+  vote_average: number;
+}
+
+export interface TMDBResponse<T> {
+  page: number;
+  results: T[];
+  total_pages: number;
+  total_results: number;
+}
